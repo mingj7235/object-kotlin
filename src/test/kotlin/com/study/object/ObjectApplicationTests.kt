9f6f3ca1501a790c173d7de0006416cad68da2fc
mingj7235 <joshuara7235@gmail.com>
2024-08-31 21:16:32 +0900

    object kotlin project init

---
+++ src/test/kotlin/com/study/object/ObjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.`object`
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
